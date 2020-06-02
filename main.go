761fb45407f34aa25da4bbc48e831bf2548905c8
gandharv-pantelwar <gandharv.pantelwar@gmail.com>
2020-06-02 20:15:26 +0530

    #34 grpc server implemented

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
-	"matching-engine/engine"
+	engineGrpc "matching-engine/engineGrpc"
+	"matching-engine/server"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	ob := engine.NewOrderBook()
-	fmt.Println("Orderbook", ob)
+	gs := grpc.NewServer()
+	cs := server.NewEngine()
+	engineGrpc.RegisterEngineServer(gs, cs)
+
+	reflection.Register(gs)
+
+	l, err := net.Listen("tcp", ":9092")
+	if err != nil {
+		_ = fmt.Errorf("Unable to listen server, err: %v", err)
+		os.Exit(1)
+	}
+	gs.Serve(l)
 }
